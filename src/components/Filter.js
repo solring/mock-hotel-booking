656216e8f7e19aa859dc5e62b7f42c5b34957feb
Solring Lin <sherry12714@gmail.com>
2021-07-05 20:06:08 +0100

    use material-ui slider.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,24 +1,15 @@
+import { useState } from 'react';
+import { Slider } from '@material-ui/core';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import * as constants from '../utils/constants';
+
+import './Filter.scss';
 
 function Filter(props) {
   const {name} = props;
 
+  const [priceRange, setPriceRange] = useState([1000,5000]);
   let filterName = name || "";
-
-  const genCheckBox = (sec, prefix) => {
-    return (
-      <div>
-        <h6 className="card-title">{sec.title}</h6>
-        <ul className="list-gap-8">
-          {sec.options.map((option) => (
-            <li key={option.name} className="custom-control custom-checkbox">
-            <input type="checkbox" className="custom-control-input" id={`${prefix}_${option.name}_Check`} name={option.name}/>
-            <label htmlFor={`${prefix}_${option.name}_Check`} className="custom-control-label text-secondary">{option.text}</label>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
 
   const sections = [
     {
@@ -78,6 +69,55 @@ function Filter(props) {
     },
   ]
 
+  /* ------- Slider -------- */
+  const CustomSlider = withStyles({
+    root: {
+      color: constants.PRIMARY_COLOR,
+      height: 2,
+      padding: '8px 0',
+    },
+    thumb: {
+      height: 16,
+      width: 16,
+      marginTop: -8,
+      marginLeft: -8,
+      backgroundColor: constants.PRIMARY_COLOR,
+      '&:focus, &:hover, &$active': {
+        boxShadow: '#aaa 0 2px 3px 1px',
+      },
+    },
+    active: {},
+    track: {
+      height: 2,
+    },
+    rail: {
+      height: 2,
+      opacity: 0.5,
+      backgroundColor: '#bfbfbf',
+    },
+  })(Slider);
+
+  const sliderHandler = (event, newValue) => {
+    setPriceRange(newValue);
+  }
+
+  /* ------ helper functions ------ */
+  const genCheckBox = (sec, prefix) => {
+    return (
+      <div>
+        <h6 className="card-title">{sec.title}</h6>
+        <ul className="list-gap-8">
+          {sec.options.map((option) => (
+            <li key={option.name} className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id={`${prefix}_${option.name}_Check`} name={option.name}/>
+            <label htmlFor={`${prefix}_${option.name}_Check`} className="custom-control-label text-secondary">{option.text}</label>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
   const genGradeForm = () => {
     let nums = [1, 2, 3, 4, 5];
 
@@ -119,14 +159,22 @@ function Filter(props) {
   <li key="sec3">
     <h6 className="card-title">Budget</h6>
 
-    <div className="my-4 budgetRange" id="{filterName}Budget"></div>
+    <CustomSlider
+      value={priceRange}
+      onChange={sliderHandler}
+      max={constants.MAX_FILTER_PRICE}
+      min={constants.MIN_FILTER_PRICE}
+      step={constants.FILTER_STEP}
+    />
 
-    <ul className="price-tag w-100">
+    <ul className="priceTag w-100">
       <li key="minPrice">
         <p className="title">min price</p>
         <div className="d-flex price">
-          <label htmlFor="minPrice">TWD </label>
-          <input type="text" readOnly id="{filterName}MinPrice" className="minPrice" name="minPrice" />
+          <span className="priceTagNum">
+            {priceRange[0]}
+          </span>
+          <span className="priceTagLabel">TWD</span>
         </div>
       </li>
       <li className="divider">
@@ -135,8 +183,10 @@ function Filter(props) {
       <li key="maxPrice">
         <p className="title">max price</p>
         <div className="d-flex price">
-          <label htmlFor="maxPrice">TWD </label>
-          <input type="text" readOnly id="{filterName}MaxPrice" className="maxPrice" name="maxPrice" />
+          <span className="priceTagNum">
+            {priceRange[1]}
+          </span>
+          <span className="priceTagLabel">TWD</span>
         </div>
       </li>
     </ul>
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const MIN_FILTER_PRICE = 0;
+export const MAX_FILTER_PRICE = 10000;
+export const FILTER_STEP = 100;
+
+export const PRIMARY_COLOR = "#6C51E2";
